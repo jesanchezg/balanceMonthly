ba35dbe2ddb96c34f5b6e45a9de8396425138a30
jesanchez <jesanchez@jesanchez-Inspiron-N5110>
2018-04-15 23:40:20 -0400

    Tab Authentication, Header component, paymentsComponent for separate the list of the payments.

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,13 +6,20 @@ import { AppComponent } from "./app.component";
 import { PaymentComponent } from "./payments/payment.component";
 import { PaymentListComponent } from "./payments/payment-list.component";
 import {PaymentInputComponent} from "./payments/payment-input.component";
+import {PaymentsComponent} from "./payments/payments.component";
+import {AuthenticationComponent} from "./users/authentication.component";
+import {HeaderComponent} from "./header.component";
+
 
 @NgModule({
     declarations: [
         AppComponent,
         PaymentComponent,
         PaymentListComponent,
-        PaymentInputComponent
+        PaymentInputComponent,
+        PaymentsComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule ],
     bootstrap: [AppComponent]
